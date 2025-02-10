4bf3280327f8196c66e9fe7de282d1ae2738b8e7
ms46326 <ms46326@gmail.com>
2025-02-10 21:42:40 +0800

    Updata 屬性

---
+++ Assets/Scripts/class_7_2_Static.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+namespace KID
+{
+    /// <summary>
+    /// 靜態 Static
+    /// </summary>
+    public class class_7_2_Static : MonoBehaviour
+    {
+        // 非靜態變數
+        public int inventorwater = 10;
+        // 靜態變數：修飾詞後面添加關鍵字 static
+        // 靜態變數不顯示在屬性面板上
+        public static int inventorProp = 20;
+
+        private void Awake()
+        {
+            inventorwater = 7;
+            inventorProp = 15;
+            Debug.Log($"<color=#f31>藥水：{inventorwater}</color>");
+            Debug.Log($"<color=#f13>道具：{inventorProp}</color>");
+        }
+    }
+}
+
