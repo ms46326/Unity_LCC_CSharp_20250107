1695b668eb48ea21715c2e485d4db0961b9a9a91
ms46326 <ms46326@gmail.com>
2025-03-10 21:49:29 +0800

    Updata 類別的泛型 20250310

--- Assets/Scripts/class_15_Generics.cs
+++ Assets/Scripts/class_15_Generics.cs
@@ -9,6 +9,7 @@ namespace KID.Class_15
     {
         private void Awake()
         {
+            #region 不使用泛型
             int numberA = 7, numberB = 9;
             LogSystem.LogWithColor($"數字 A 與 B：{numberA}|{numberB}", "#f93");
             SwapNumber(ref numberA, ref numberB);
@@ -19,20 +20,42 @@ namespace KID.Class_15
             SwapChar(ref charA, ref charB);
             LogSystem.LogWithColor($"字元 A 與 B：{charA}|{charB}", "#9f3");
 
-            object objA = 3.5f, objB = 7.7f;
+            object objA = 3.5f, objB = '試';
             LogSystem.LogWithColor($"物件 A 與 B：{objA}|{objB}", "#3f3");
             SwapObject(ref objA, ref objB);
             LogSystem.LogWithColor($"物件 A 與 B：{objA}|{objB}", "#3f3");
+            #endregion
 
+            #region 使用泛型
+            bool boolA = true, boolB = false;
+            LogSystem.LogWithColor($"布林值 A 與 B：{boolA}|{boolB}", "#9f9");
+            Swap<bool>(ref boolA, ref boolB);
+            LogSystem.LogWithColor($"布林值 A 與 B：{boolA}|{boolB}", "#9f9");
+
+            byte byteA = 1, byteB = 9;
+            LogSystem.LogWithColor($"byte A 與 B：{byteA}|{byteB}", "#99f");
+            Swap<byte>(ref byteA, ref byteB);
+            LogSystem.LogWithColor($"byte A 與 B：{byteA}|{byteB}", "#99f");
+            #endregion
+
+            // 泛型-類別
+            var player1 = new DataPlayer<int>();
+            player1.data = 99;
+            player1.LogData(123);
+
+            var player2 = new DataPlayer<string>();
+            player2.data = "玩家二號";
+            player2.LogData("哈囉");
         }
 
+        #region 方法區域
 
         /// <summary>
         /// 數字對調
         /// </summary>
         /// <param name="a">第一個數字</param>
         /// <param name="b">第二個數字</param>
-        public void SwapNumber(ref int a,ref int b)
+        public void SwapNumber(ref int a, ref int b)
         {
             int temp = a;   // 將第一個數字放去旁邊(暫存)
             a = b;          // 將第二個數字放到第一個數字內
@@ -44,7 +67,7 @@ namespace KID.Class_15
         /// </summary>
         /// <param name="a">第一個字元</param>
         /// <param name="b">第二個字元</param>
-        public void SwapChar(ref char a, ref char b) 
+        public void SwapChar(ref char a, ref char b)
         {
             char temp = a;
             a = b;
@@ -62,6 +85,32 @@ namespace KID.Class_15
             a = b;
             b = temp;
         }
+
+        /// <summary>
+        /// 使用泛型進行對調
+        /// </summary>
+        /// <typeparam name="T">要對調的資料類型</typeparam>
+        /// <param name="a">第一個資料</param>
+        /// <param name="b">第二個資料</param>
+        public void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        } 
+        #endregion
     }
+
+    public class DataPlayer<T> 
+    {
+        public T data;
+
+        public void LogData(T data) 
+        {
+            LogSystem.LogWithColor(data, "#3ff");
+        }
+    }
+
+
 }
 
