4f20895001ffacd2b0fadef2b7efb93f2a8f892e
ms46326 <ms46326@gmail.com>
2025-03-10 21:05:44 +0800

    Updata 介面與泛型 20250310

---
+++ Assets/Scripts/class_15_Generics.cs
@@ -0,0 +1,67 @@
+﻿using AES.Tools;
+using UnityEngine;
+namespace KID.Class_15
+{
+    /// <summary>
+    /// 泛型 Generics
+    /// </summary>
+    public class class_15_Generics : MonoBehaviour
+    {
+        private void Awake()
+        {
+            int numberA = 7, numberB = 9;
+            LogSystem.LogWithColor($"數字 A 與 B：{numberA}|{numberB}", "#f93");
+            SwapNumber(ref numberA, ref numberB);
+            LogSystem.LogWithColor($"數字 A 與 B：{numberA}|{numberB}", "#f93");
+
+            char charA = '嗨', charB = '嘿';
+            LogSystem.LogWithColor($"字元 A 與 B：{charA}|{charB}", "#9f3");
+            SwapChar(ref charA, ref charB);
+            LogSystem.LogWithColor($"字元 A 與 B：{charA}|{charB}", "#9f3");
+
+            object objA = 3.5f, objB = 7.7f;
+            LogSystem.LogWithColor($"物件 A 與 B：{objA}|{objB}", "#3f3");
+            SwapObject(ref objA, ref objB);
+            LogSystem.LogWithColor($"物件 A 與 B：{objA}|{objB}", "#3f3");
+
+        }
+
+
+        /// <summary>
+        /// 數字對調
+        /// </summary>
+        /// <param name="a">第一個數字</param>
+        /// <param name="b">第二個數字</param>
+        public void SwapNumber(ref int a,ref int b)
+        {
+            int temp = a;   // 將第一個數字放去旁邊(暫存)
+            a = b;          // 將第二個數字放到第一個數字內
+            b = temp;       // 將旁邊的數字放到第二個數字
+        }
+
+        /// <summary>
+        /// 字元對調
+        /// </summary>
+        /// <param name="a">第一個字元</param>
+        /// <param name="b">第二個字元</param>
+        public void SwapChar(ref char a, ref char b) 
+        {
+            char temp = a;
+            a = b;
+            b = temp;
+        }
+
+        /// <summary>
+        /// 物件對調
+        /// </summary>
+        /// <param name="a">第一個物件</param>
+        /// <param name="b">第二個物件</param>
+        public void SwapObject(ref object a, ref object b)  // 不建議 object，因為封裝、拆裝消耗系統資源
+        {
+            object temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
+
---
+++ Assets/Scripts/class_15_interfaceObject.cs
@@ -0,0 +1,72 @@
+﻿using AES.Tools;
+using UnityEngine;
+namespace KID.Class_15
+{
+    /// <summary>
+    /// 介面物件：實作介面的物件
+    /// </summary>
+    public class class_15_interfaceObject
+    {
+
+    }
+
+    // 介面的使用步驟
+    // 1.定義介面與介面成員
+    // 2.類別實作介面
+    // 3.實作介面的成員
+
+    // 步驟1.
+    // 帶有「使用」功能的介面，遊戲內可被使用的物品
+    public interface IUse   // 淺規則：只要是 I 開頭都是介面
+    {
+        // 不用宣告方法主體，不用寫大括號
+        public void Use();
+    }
+
+    public interface IDestory
+    {
+        public void Destory();
+    }
+
+    // 步驟2
+    // C# 單一繼承，多重實作介面
+    public class Weapon : IUse  //這時候IUse會出現紅蚯蚓，點紅蚯蚓就是步驟3.
+    {
+        // 步驟3.
+        public void Use()
+        {
+            // throw new System.NotImplementedException();  // 這行要刪掉，不然會出現錯誤
+            LogSystem.LogWithColor("使用武器，施展武器技能", "#fa3");
+        }
+    }
+
+    public class Potion : IUse , IDestory   //實作多重介面，實作第二個介面
+    {
+        public void Destory()
+        {
+            LogSystem.LogWithColor("藥水使用完畢，刪除", "#f11");
+        }
+
+        public void Use() 
+        {
+            LogSystem.LogWithColor("使用藥水，恢復體力", "#a3f");
+        }
+    }
+
+    public class Chest : IUse, IDestory
+    {
+        public void Destory()
+        {
+            LogSystem.LogWithColor("寶箱使用完畢，刪除", "#f11");
+        }
+
+        public void Use()
+        {
+            LogSystem.LogWithColor("使用寶箱，獲得隨機道具", "#af3");
+        }
+    }
+
+
+
+}
+
